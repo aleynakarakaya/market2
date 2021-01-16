185a20938df61a8275e3ebe61ce014415ca2f8b3
aleynakarakaya <alynkrky34@gmail.com>
2021-01-16 18:21:52 +0300

    vs

---
+++ lib/tabs/search_tab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SearchTab extends StatefulWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Search Tab"),
+     ),
+    );
+  }
+}
